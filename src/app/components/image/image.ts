1baa94d373b203fec3c035310e2d91912af0ea75
Brian <45598654+BrianStiv@users.noreply.github.com>
2026-05-05 02:52:14 +0200

    feat: create Image component with selection and delete

---
+++ src/app/components/image/image.ts
@@ -0,0 +1,40 @@
+import { Component, input, output, ChangeDetectionStrategy } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { ImageModel } from '../../models/imageModel';
+import { CheckboxModule } from 'primeng/checkbox';
+
+@Component({
+  selector: 'app-image',
+  standalone: true,
+  imports: [CommonModule, FormsModule, CheckboxModule],
+  templateUrl: './image.html',
+  changeDetection: ChangeDetectionStrategy.OnPush,
+  host: {
+    'class': 'block relative w-full h-full'
+  }
+})
+export class ImageComponent {
+
+  image = input.required<ImageModel>();
+  isSelected = input<boolean>(false);
+  isFeatured = input<boolean>(false);
+
+  deleteImage = output<ImageModel>();
+  selectImage = output<ImageModel>();
+
+
+  toggleSelection(event: Event) {
+    event.stopPropagation();
+    this.selectImage.emit(this.image());
+  }
+
+  deleteImageClick(event: Event) {
+    event.stopPropagation();
+    this.deleteImage.emit(this.image());
+  }
+
+  onCheckboxChange() {
+    this.selectImage.emit(this.image());
+  }
+}
